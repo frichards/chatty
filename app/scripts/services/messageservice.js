ddc97ca14e2074ca61c066d067d8da52a6ad9c35
Fumiko Richards <fumikom@gmail.com>
2014-06-16 17:38:47 -0600

    complete

--- app/scripts/services/messageservice.js
+++ app/scripts/services/messageservice.js
@@ -1,7 +1,38 @@
 'use strict';
 
 angular.module('chattyApp')
-  .service('MessageService', function MessageService() {
-    return {
-    }
+  .service('MessageService', function MessageService($http, $q) {
+ 
+  	this.getMessages =  function(){
+			var d = $q.defer();
+			$http({
+				method:'GET',
+				url: 'http://localhost:11100/'
+			}).success(function(data){
+				d.resolve(data);
+
+			}).error(function (err){
+				d.reject(err);
+			});
+
+		return d.promise;
+		};
+
+	 this.postMessages= function(text){
+			var d = $q.defer();
+			$http({
+				method:'POST',
+				url: 'http://localhost:11100',
+				data: {message: text}
+			}).success(function(data){
+				d.resolve(data);
+
+			}).error(function (err){
+				d.reject(err);
+			});
+
+		return d.promise;
+		};
+
   });
+
